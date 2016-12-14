75311c667999aed39b8c5f9dacc5952e662af4bd
bitshift <bitshift@posteo.org>
2016-12-14 02:57:23 +0100

    Adds more unit tests and further details exisiting ones

--- unit-test/src/main.cpp
+++ unit-test/src/main.cpp
@@ -10,34 +10,38 @@ using namespace source;
 
 TEST_CASE( "image_source returns path as id", "[image_source]" ) 
 {
-	ImageSource source{ "concept.img" };
-	REQUIRE( source.id() == "concept.img" );
+	string path{ "data/sources/images/image1.jpg" };
+	ImageSource source{ path };
+	REQUIRE( source.id() == path );
 }
 
 TEST_CASE( "fbo_source returns name as id", "[fbo_source]" ) 
 {
-	FboSource source{"fbo #1"};
+	fbo_source source{ "fbo #1" };
 	REQUIRE( source.id() == "fbo #1" );
 }
 
 TEST_CASE( "stream_source returns url as id", "[stream_source]" ) 
 {
-	stream_source source{"url"};
+	stream_source source{ "url" };
 	REQUIRE( source.id() == "url" );
 }
 
-TEST_CASE( "source_forwarder can be created", "[source_forwarder]" ) 
+TEST_CASE( "source_forwarder can be created returns path as id", "[source_forwarder]" ) 
 {
 	using source_ptr_t = shared_ptr<ImageSource>;
 	using source_fwd_t = SourceForwarder<source_ptr_t>;
 
-	source_ptr_t source_ptr(new ImageSource("image.png"));
-	source_fwd_t fwd(source_ptr);
+	string path{ "data/sources/images/image1.jpg" };
+	source_ptr_t source_ptr( new ImageSource( path ));
+	source_fwd_t fwd( source_ptr );
+	REQUIRE( fwd.id() == path );
 }
 
 TEST_CASE( "source_model can be created from image_source", "[source_model]" ) 
 {
-	ImageSource imageSource{"concept.img"};
+	string path{ "data/sources/images/image1.jpg" };
+	ImageSource imageSource{ path };
 	
 	auto imageModel = new SourceModel<ImageSource>(move(imageSource));
 	auto source = shared_ptr<Source>(imageModel);
