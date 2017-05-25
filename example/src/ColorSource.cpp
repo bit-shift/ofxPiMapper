2204de6d07e32c1be5fa5ec28db741a4707ec1e3
bitshift <bitshift@posteo.org>
2017-05-25 15:34:06 +0200

    Changes Tweenzor parameters.

--- example/src/ColorSource.cpp
+++ example/src/ColorSource.cpp
@@ -12,7 +12,7 @@ void ColorSource::setup() {
     ofSetFrameRate(60);  
     Tweenzor::init();    
     _lerpAmount = 255.f;  
-    Tweenzor::add(&_lerpAmount, 0.f, 255.f, 0.f, 0.5f);  
+    Tweenzor::add(&_lerpAmount, 0.f, 255.f, 0.f, 10.f);  
     Tweenzor::getTween(&_lerpAmount)->setRepeat(-1, true);  
 }  
 
@@ -24,6 +24,7 @@ void ColorSource::draw(){
 	ofClear(0);
 
 	ofSetColor(255, 40, _lerpAmount);
+	ofSetColor(_lerpAmount, 40, _lerpAmount);
 
 	#if (OF_VERSION_MAJOR == 0 && OF_VERSION_MINOR >= 9) || OF_VERSION_MAJOR > 0
 		ofDrawRectangle(0, 0, getWidth(), getHeight());
